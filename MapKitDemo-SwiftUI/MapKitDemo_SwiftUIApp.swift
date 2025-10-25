d9020b95263bacd0411f51dc6a093be45368e4ee
Aditya Ghadge <adityasghadge@gmail.com>
2025-10-25 09:36:46 +0530

    Initial Commit

---
+++ MapKitDemo-SwiftUI/MapKitDemo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitDemo_SwiftUIApp.swift
+//  MapKitDemo-SwiftUI
+//
+//  Created by Aditya on 25/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MapKitDemo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
